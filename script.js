c0787215a32d7f3d5bad3c60a10436ada92f1c8f
heywhyyou <havenoreasontolive@gmail.com>
2023-03-28 00:40:41 +0300

    chore: test some DOM interactions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Some DOM testing:
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🍺 Correct Number';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
