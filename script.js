e8cf35657dfe3635c9a5dae95f2985ea26904227
heywhyyou <havenoreasontolive@gmail.com>
2023-03-28 01:04:28 +0300

    feat: add correct num check

--- script.js
+++ script.js
@@ -10,8 +10,8 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,5 +19,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '🙈 No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '💕 Correct Number';
   }
 });
