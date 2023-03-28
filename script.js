5b987b97fda6e243e028be23cbd898f22a195c26
heywhyyou <havenoreasontolive@gmail.com>
2023-03-29 00:15:48 +0300

    feat: add style changes on correct guess

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '💕 Correct Number';
 
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
